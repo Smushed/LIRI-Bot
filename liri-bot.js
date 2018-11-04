8a5d844c4e8191d2d797cffdf8cec8a9d0eabb3b
Smushed <smushedcode@gmail.com>
2018-11-04 07:57:34 -0600

    I have the OMDB API working well. Also included request-promise to make use of .then and .catch when sending the request to OMDB API

--- liri-bot.js
+++ liri-bot.js
@@ -3,46 +3,100 @@
  * API KEYS ECT
  */
 //What the app requires, gathering the API Keys and placing them correctly
-require("dotenv").config()
-const spotifyKeys = require("./keys.js")
+require("dotenv").config();
+const rq = require("request");
+//Must also install request-promise so I can use .then and .catch when querying APIs using request
+//Not 100% nessessary but it makes readability much easier
+const request = require("request-promise");
+const spotifyKeys = require("./keys.js");
 const Spotify = require("node-spotify-api")
 
 //config the Spotify API
 const spotify = new Spotify({
     id: spotifyKeys.spotifyAPIInfo.id,
     secret: spotifyKeys.spotifyAPIInfo.secret
-})
+});
 
 // get the user input
 
 const liriCommand = process.argv[2];
-let input = process.argv[3];
+const input = process.argv
+//Combined input is if the user inputs spaces and it combines them in the for loop below
+let combinedInput = "";
 
-if (input === undefined) {
-    input = "The Sign, Ace of Base"
-}
+//For loop so the user can include spaces in their request
+for (let i = 3; i < input.length; i++) {
+    if (i === 3) {
+        combinedInput = input[i];
+    } else {
+        combinedInput = combinedInput + " " + input[i];
+    }
+};
+
 
 const concertThis = function () {
-    console.log("Concert This")
-}
+    console.log("Concert This");
+};
 
 const spotifyThisSong = function () {
-    spotify.search({ type: "track", query: input, limit: 1 })
-        .then(function (response) {
+    //There has to be a better way to do this
+    if (combinedInput === "") {
+        combinedInput = "The Sign, Ace of Base";
+    };
+
+    spotify.search({ type: "track", query: combinedInput, limit: 1 })
+        .then(response => {
             //Parsing the response object and assigning it to variables for readability
             const artist = response.tracks.items[0].artists[0].name;
             const songName = response.tracks.items[0].name;
             const spotifyURL = response.tracks.items[0].external_urls.spotify;
             const album = response.tracks.items[0].album.name;
 
+            //Logging the requested song to the console
             console.log(`Artist: ${artist}`);
             console.log(`Song Name: ${songName}`);
             console.log(`URL: ${spotifyURL}`);
             console.log(`Album: ${album}`);
-        }).catch(function (err) {
+        }).catch(err => {
             console.log(err);
         });
 };
+
+const movieThis = function () {
+    if (combinedInput === "") {
+        combinedInput = "Mr. Nobody";
+    };
+    const query = {
+        uri: "http://www.omdbapi.com/?t=" + combinedInput + "&y=&plot=short&apikey=trilogy",
+        json: true
+    }
+    //Queries the OMDB API for the movie title that the user provided
+    request(query)
+        .then(response => {
+
+            //Parsing the response object and assigning it to variables for readability
+            const movieTitle = response.Title;
+            const movieYear = response.Year;
+            const IMDBRating = response.Ratings[0].Value;
+            const RTRating = response.Ratings[1].Value;
+            const movieCountry = response.Country;
+            const movieLanguage = response.Language;
+            const moviePlot = response.Plot;
+            const movieActors = response.Actors;
+
+            console.log(`Title: ${movieTitle}`);
+            console.log(`Year: ${movieYear}`);
+            console.log(`IMDB Rating: ${IMDBRating}`);
+            console.log(`Rotten Tomatoes Rating: ${RTRating}`);
+            console.log(`Country Produced: ${movieCountry}`);
+            console.log(`Language: ${movieLanguage}`);
+            console.log(`Plot: ${moviePlot}`);
+            console.log(`Actors: ${movieActors}`);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
 
 switch (liriCommand) {
     case "concert-this":
@@ -58,6 +112,6 @@ switch (liriCommand) {
         doWhatItSays();
         break;
     default:
-        console.log("I don't understand")
-}
+        console.log("I don't understand");
+};
 
