bb0699d2e2f932374a1ca767011ae64b397f620e
Smushed <smushedcode@gmail.com>
2018-11-04 20:27:16 -0600

    The Bands in Town API is now working, moving onto the do what it says portion

--- liri-bot.js
+++ liri-bot.js
@@ -3,14 +3,15 @@
  * API KEYS ECT
  */
 //What the app requires, gathering the API Keys and placing them correctly
-require("dotenv").config();
+require(`dotenv`).config();
 /**Using request-promise so I can use .then and .catch when querying APIs using request
 *Must Install request & request-promise modules in order for this to work
 *Not 100 % nessessary but it makes readability much easier
 */
-const request = require("request-promise");
-const spotifyKeys = require("./keys.js");
-const Spotify = require("node-spotify-api")
+const request = require(`request-promise`);
+const spotifyKeys = require(`./keys.js`);
+const Spotify = require(`node-spotify-api`);
+const moment = require(`moment`);
 
 //config the Spotify API
 const spotify = new Spotify({
@@ -18,34 +19,68 @@ const spotify = new Spotify({
     secret: spotifyKeys.spotifyAPIInfo.secret
 });
 
-// get the user input
-
+// Grabbing the user input from the command line
 const liriCommand = process.argv[2];
 const input = process.argv
 //Combined input is if the user inputs spaces and it combines them in the for loop below
-let combinedInput = "";
+let combinedInput = ``;
 
 //For loop so the user can include spaces in their request
 for (let i = 3; i < input.length; i++) {
     if (i === 3) {
         combinedInput = input[i];
     } else {
-        combinedInput = combinedInput + " " + input[i];
+        combinedInput = combinedInput + ` ` + input[i];
     }
 };
 
 
 const concertThis = function () {
-    console.log("Concert This");
+    if (combinedInput === ``) {
+        combinedInput = `Kiss`;
+    };
+    const query = {
+        uri: `https://rest.bandsintown.com/artists/${combinedInput}/events?app_id=codingbootcamp`,
+        json: true
+    }
+    request(query)
+        .then(response => {
+            //Checks if the response has come back with data. If it has not the artist has no upcoming shows
+            if (response.length == 0) {
+                console.log(`The band has no upcoming shows`)
+            } else {
+                //Parsing the reponse and assigning it to variables for readability
+                let lineup = ``;
+                const bandDate = moment(response[0].datetime).format(`MM/DD/YYYY`);
+                const bandVenue = response[0].venue.name;
+                const venueLocation = `${response[0].venue.city}, ${response[0].venue.region}`;
+
+                //For Loop to parse the lineup array to return it
+                for (let i = 0; i < response[0].lineup.length; i++) {
+                    if (i === 0) {
+                        lineup = response[0].lineup[i];
+                    } else {
+                        lineup = `, ${response[0].lineup[i]}`;
+                    };
+                }
+
+                console.log(`Lineup: ${lineup}`)
+                console.log(`Date: ${bandDate}`);
+                console.log(`Venue: ${bandVenue}`);
+                console.log(`Location: ${venueLocation}`);
+            }
+        }).catch(err => {
+            console.log(err);
+        });
 };
 
 const spotifyThisSong = function () {
-    //There has to be a better way to do this
-    if (combinedInput === "") {
-        combinedInput = "The Sign, Ace of Base";
+
+    if (combinedInput === ``) {
+        combinedInput = `The Sign, Ace of Base`;
     };
 
-    spotify.search({ type: "track", query: combinedInput, limit: 1 })
+    spotify.search({ type: `track`, query: combinedInput, limit: 1 })
         .then(response => {
             //Parsing the response object and assigning it to variables for readability
             const artist = response.tracks.items[0].artists[0].name;
@@ -64,11 +99,12 @@ const spotifyThisSong = function () {
 };
 
 const movieThis = function () {
-    if (combinedInput === "") {
-        combinedInput = "Mr. Nobody";
+
+    if (combinedInput === ``) {
+        combinedInput = `Mr. Nobody`;
     };
     const query = {
-        uri: "http://www.omdbapi.com/?t=" + combinedInput + "&y=&plot=short&apikey=trilogy",
+        uri: `http://www.omdbapi.com/?t=${combinedInput}&y=&plot=short&apikey=trilogy`,
         json: true
     }
     //Queries the OMDB API for the movie title that the user provided
@@ -99,19 +135,19 @@ const movieThis = function () {
 }
 
 switch (liriCommand) {
-    case "concert-this":
+    case `concert-this`:
         concertThis();
         break;
-    case "spotify-this-song":
+    case `spotify-this-song`:
         spotifyThisSong();
         break;
-    case "movie-this":
+    case `movie-this`:
         movieThis();
         break;
-    case "do-what-it-says":
+    case `do-what-it-says`:
         doWhatItSays();
         break;
     default:
-        console.log("I don't understand");
+        console.log(`I don't understand`);
 };
 
